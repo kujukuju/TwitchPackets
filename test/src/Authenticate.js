d169ed3983ca6ac1d2ab946bac2ae47313d00cd7
Trent Davies <trentwdb@gmail.com>
2019-11-18 22:01:00 -0800

    cleaned up the authenticate file so theres no overlap with twitchpackets library

--- test/src/Authenticate.js
+++ test/src/Authenticate.js
@@ -13,14 +13,6 @@ class Authenticate {
     }
 
     static authenticate(clientID, secret, code) {
-        return Authenticate._getToken(clientID, secret, code);
-    }
-
-    static refresh(clientID, secret, refreshToken) {
-        return Authenticate._refreshToken(clientID, secret, refreshToken);
-    }
-
-    static _getToken(clientID, secret, code) {
         return new Promise((resolve, reject) => {
             const request = new XMLHttpRequest();
             request.onload = () => {
@@ -38,29 +30,6 @@ class Authenticate {
             };
 
             const params = 'client_id=' + clientID + '&client_secret=' + secret + '&code=' + code + '&grant_type=authorization_code&redirect_uri=' + encodeURIComponent(Authenticate.REDIRECT_URL);
-            request.open('POST', 'https://id.twitch.tv/oauth2/token?' + params, true);
-            request.send();
-        });
-    }
-
-    static _refreshToken(clientID, secret, refreshToken) {
-        return new Promise((resolve, reject) => {
-            const request = new XMLHttpRequest();
-            request.onload = () => {
-                if (request.status !== 200) {
-                    console.log('Request completed with an incorrect status. ', request.status);
-                    return reject(request.responseText);
-                }
-
-                console.log('Refresh authentication completed.');
-                console.log(JSON.parse(request.responseText));
-                return resolve(JSON.parse(request.responseText));
-            };
-            request.onerror = () => {
-                return reject(request.responseText);
-            };
-
-            const params = 'grant_type=refresh_token&refresh_token=' + refreshToken + '&client_id=' + clientID + '&client_secret=' + secret;
             request.open('POST', 'https://id.twitch.tv/oauth2/token?' + params, true);
             request.send();
         });
